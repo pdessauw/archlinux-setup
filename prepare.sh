5f3177645c97b3939fbcd8c97771ae7911917b59
pdessauw <pdessauw@gmail.com>
2022-03-09 15:49:19 -0500

    feat: cleanup prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Pre-installation steps for an Archlinux machine.
+#
+# This script is adapted from:
+#   https://wiki.archlinux.org/title/Installation_guide#Pre-installation
 #
 # ===== Edit the following values =====
 BOOT_SIZE="250MiB"
@@ -7,22 +11,30 @@ HDD_PATH="/dev/sda"
 
 # ==== DO NOT EDIT  PAST THIS LINE ====
 # -------------------------------------
+# Update system clock.
 timedatectl set-ntp true
 
-parted -s ${HDD_PATH} mklabel msdos mkpart primary ext4 1MiB ${BOOT_SIZE} \
+# Partition the disk.
+parted -s ${HDD_PATH} \
+  mklabel msdos \
+  mkpart primary ext4 1MiB ${BOOT_SIZE} \
   set 1 boot on \
   mkpart primary linux-swap ${BOOT_SIZE} ${SWAP_SIZE} \
   mkpart primary ext4 ${SWAP_SIZE} 100%
-  
+
+# Format the partitions.
 mkfs.ext4 ${HDD_PATH}1
 mkswap ${HDD_PATH}2
 mkfs.ext4 ${HDD_PATH}3
 
+# Mount the filesystem.
 mount ${HDD_PATH}3 /mnt
 mkdir /mnt/boot
 mount ${HDD_PATH}1 /mnt/boot
 
+# Install the essentials.
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Configuration finished. Please run 'arch-chroot /mnt' to continue the installation."
+echo "Configuration finished. Please run 'arch-chroot /mnt' to continue the " \
+  "installation."
