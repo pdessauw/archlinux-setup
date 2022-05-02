03d414ae46ddedac0c0fe034214741b071c3acd8
pdessauw <pdessauw@gmail.com>
2022-05-02 10:39:19 -0400

    fix: use abs path for install dir

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,5 +2,6 @@
 # Script performing the system installation using Ansible.
 # =========================
 set -e
-INSTALL_DIR="install.d"
-find ${INSTALL_DIR} -name "*.sh" -exec bash {} \;
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_DIR="${SCRIPT_DIR}/install.d"
+find "${INSTALL_DIR}" -name "*.sh" -exec bash {} \;
