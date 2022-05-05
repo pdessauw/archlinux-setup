3717e309c953e7d7e1ed38f99a34405d41a86317
pdessauw <pdessauw@gmail.com>
2022-05-05 16:34:49 -0400

    fix: issues with arch-root inline script

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#
+# Script preparing the machine for the Ansible install.
 # ===== Edit the following values =====
 BOOT_SIZE="250MiB"
 SWAP_SIZE="4GiB"
 HDD_PATH="/dev/sda"
-ADMIN_PASSWORD="*****"
+ROOT_PASSWORD="*****"
 
 # ==== DO NOT EDIT  PAST THIS LINE ====
 # -------------------------------------
@@ -26,12 +26,18 @@ mount ${HDD_PATH}1 /mnt/boot
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
+GIT_REPO="/root/archlinux-setup"
 arch-chroot /mnt bash -c """
-  GIT_REPO='/root/archlinux-setup'
   pacman -S --noconfirm vim git
   git clone https://github.com/pdessauw/archlinux-setup ${GIT_REPO}
+
   sed -i \
-    -E 's;(ADMIN_PASSWORD=\")[^\"]+\";\1${ADMIN_PASSWORD}\";'
+    -E 's;(ROOT_PASSWORD=\")[^\"]+\";\1${ROOT_PASSWORD}\";' \
     ${GIT_REPO}/install.d/root.sh
+
   ${GIT_REPO}/install-ansible.sh
+
+  sed -i \
+    -E 's;(ROOT_PASSWORD=\")[^\"]+\";\1*****\";' \
+    ${GIT_REPO}/install.d/root.sh
 """
