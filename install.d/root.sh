1a1473eb0a458adc74d1a5b89e20309d98daeea7
pdessauw <pdessauw@gmail.com>
2022-05-06 10:08:47 -0400

    fix: password change error

--- install.d/root.sh
+++ install.d/root.sh
@@ -7,4 +7,4 @@ ROOT_PASSWORD="*****"  # WARNING: the value will be overwritten.
 cat "${SCRIPT_DIR}/../data/bash_profile_add" >> /root/.bash_profile
 cat "${SCRIPT_DIR}/../data/rc.sh" >> /root/.bashrc
 
-echo -e "${ROOT_PASSWORD}/n${ROOT_PASSWORD}" | passwd &> /dev/null
+echo -e "${ROOT_PASSWORD}\n${ROOT_PASSWORD}" | passwd &> /dev/null
