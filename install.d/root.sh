28092e18ccefd52398ba5e29044792b823be10ba
pdessauw <pdessauw@gmail.com>
2022-05-06 10:04:45 -0400

    fix: add +x flags to root user config and resolve path issues

--- install.d/root.sh
+++ install.d/root.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_PASSWORD="*****"  # WARNING: the value will be overwritten.
 
-cat "${SCRIPT_DIR}/data/bash_profile_add" >> /root/.bash_profile
-cat "${SCRIPT_DIR}/data/rc.sh" >> /root/.bashrc
+cat "${SCRIPT_DIR}/../data/bash_profile_add" >> /root/.bash_profile
+cat "${SCRIPT_DIR}/../data/rc.sh" >> /root/.bashrc
 
 echo -e "${ROOT_PASSWORD}/n${ROOT_PASSWORD}" | passwd &> /dev/null
